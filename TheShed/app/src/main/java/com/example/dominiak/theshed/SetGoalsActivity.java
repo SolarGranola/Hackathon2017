b64c4e09039293aa5d34287c355b9e0f27a7b5d2
Justin Dominiak <justinmdominiak@gmail.com>
2017-02-04 17:55:16 -0800

    Added app activities

---
+++ TheShed/app/src/main/java/com/example/dominiak/theshed/SetGoalsActivity.java
@@ -0,0 +1,14 @@
+package com.example.dominiak.theshed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetGoalsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_set_goals);
+  }
+}
